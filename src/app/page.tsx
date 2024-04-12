32dca827ce6221792f15844bee39486336686cad
Ray Lawrence Henri Sison <raylawrencehenrisison@gmail.com>
2024-04-12 15:40:50 +0800

    Add: For Sale Sign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import { ForSaleSign } from "@/components/for-sale-sign";
 import { TopNav } from "@/components/top-nav";
 
 export default async function Home() {
   return (
     <>
-      <TopNav />
-      <main className="mx-auto max-w-[2560px] w-full h-[1440px]">
-
-      </main>
+      <div className="mx-auto max-w-[2560px] w-full h-[1440px]">
+        <TopNav />
+        <main className="relative">
+          <div className="absolute top-[344px] left-[1070px]">
+            <ForSaleSign />
+          </div>
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/for-sale-sign.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Sprite } from './sprite';
+
+export const ForSaleSign: React.FC = () => {
+  return (
+    <div className="relative">
+      <div className="absolute top-[2px] left-[33px] w-[2px] h-[369px] border-[4px] border-blue z-[2]" />
+      <div className="absolute top-[33.08px] left-[0.12px] w-[380px] h-[2px] border-[4px] border-blue z-[2]" />
+      <div className="absolute top-[2px] left-[33px] w-[347px] h-[33px] z-[1]">
+        <div className="absolute top-0 left-0 w-full h-full bg-white opacity-[0.66] z-[1]" />
+        <div className="absolute top-0 left-[1px] w-[346px] h-[30px] z-[2]">
+          <h4 className="font-light text-center text-[23px]">Public Auction</h4>
+        </div>
+        <div className="absolute top-[9px] left-[16px] z-[2]">
+          <Sprite
+            name="home"
+            width={18}
+            height={17}
+          />
+        </div>
+      </div>
+      <div className="absolute top-[50.14px] left-[50.18px] w-[330.5px] h-[181.5px] border-[4.42px] border-blue border-center rounded-[17.66px] overflow-hidden">
+        <div className="absolute top-0 left-0 bg-white opacity-[0.86] w-full h-full z-[1]" />
+        <div className="relative z-[2]">
+          <h4 className="mt-[20.05px] text-center text-[21px] font-light tracking-[2.33px]">
+            Market Value $40,0000
+          </h4>
+          <h3 className="mt-[24.25px] font-extralight text-center">
+            <span className="text-[22px] tracking-[1.83px]">14 Flood St.</span>
+            <br />
+            <span className="text-[15px] tracking-[2px]">New Orleans, LA 70122</span>
+          </h3>
+          <h5 className="mt-[20.03px] text-[14.4px] text-center tracking-[1.6px] font-extralight">
+            Reserve Bid $5,000
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+}
