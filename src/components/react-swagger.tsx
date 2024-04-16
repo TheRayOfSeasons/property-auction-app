10fbc17aa0ba5cbb05e1d802240496d6e2fded9a
Ray Lawrence Henri Sison <raylawrencehenrisison@gmail.com>
2024-04-16 18:07:57 +0800

    Fix: import of swagger-ui-react

--- src/components/react-swagger.tsx
+++ src/components/react-swagger.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import React from 'react';
-import SwaggerUI from 'swagger-ui-react';
+import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
+
+// We import swagger-ui-react this way since it uses class components
+// internally. Class components do not work with server components;
+// in which, the page we intend to use this is.
+const DynamicSwaggerUI = dynamic(() => import('swagger-ui-react'), {
+  ssr: false,
+});
 
 type Props = {
   spec: Record<string, any>,
 };
 
 export const ReactSwagger: React.FC<Props> = ({ spec }) => (
-  <SwaggerUI spec={spec} />
+  <DynamicSwaggerUI spec={spec} />
 );
